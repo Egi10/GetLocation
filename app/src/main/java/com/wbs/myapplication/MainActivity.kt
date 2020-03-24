1d6d2b035075821f77fbfdcbac0c1337a8c37e26
wowbid <egi@wowbid.live>
2020-03-24 21:05:28 +0700

    add permission and show to text view

--- app/src/main/java/com/wbs/myapplication/MainActivity.kt
+++ app/src/main/java/com/wbs/myapplication/MainActivity.kt
@@ -1,18 +1,56 @@
 package com.wbs.myapplication
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
-import android.widget.Toast
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        getLocation { latitude, longitude ->
-            Toast.makeText(applicationContext, latitude.toString(), Toast.LENGTH_LONG).show()
-            Toast.makeText(applicationContext, longitude.toString(), Toast.LENGTH_LONG).show()
+        checkPermission()
+
+        btnLocation.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v?.id == R.id.btnLocation) {
+            getLocation { latitude, longitude ->
+                tvLatitude.text = String.format("Latitude %s", latitude.toString())
+                tvLongitude.text = String.format("Longitude %s", longitude.toString())
+            }
+        }
+    }
+
+    private fun checkPermission() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                    arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 200)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
         }
     }
 }
